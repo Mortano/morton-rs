3303d02014bd5f9051c78751a153e5a95c3db51f
Pascal <pascal.bormann@igd.fraunhofer.de>
2022-02-14 15:47:46 +0100

    Working on static storage

--- src/indices/morton_index_2d.rs
+++ src/indices/morton_index_2d.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::fmt::Debug;
 use std::hash::Hash;
 
@@ -10,6 +11,12 @@ pub type FixedDepthMortonIndex2D16 = MortonIndex2D<FixedDepthStorage2D<u16>>;
 pub type FixedDepthMortonIndex2D32 = MortonIndex2D<FixedDepthStorage2D<u32>>;
 pub type FixedDepthMortonIndex2D64 = MortonIndex2D<FixedDepthStorage2D<u64>>;
 pub type FixedDepthMortonIndex2D128 = MortonIndex2D<FixedDepthStorage2D<u128>>;
+
+pub type StaticMortonIndex2D8 = MortonIndex2D<StaticStorage2D<u8>>;
+pub type StaticMortonIndex2D16 = MortonIndex2D<StaticStorage2D<u16>>;
+pub type StaticMortonIndex2D32 = MortonIndex2D<StaticStorage2D<u32>>;
+pub type StaticMortonIndex2D64 = MortonIndex2D<StaticStorage2D<u64>>;
+pub type StaticMortonIndex2D128 = MortonIndex2D<StaticStorage2D<u128>>;
 
 pub trait Storage2D : Default + PartialOrd + Ord + PartialEq + Eq + Debug + Hash {
     fn fixed_depth() -> Option<usize>;
@@ -26,9 +33,41 @@ pub struct MortonIndex2D<S: Storage2D> {
     storage: S,
 }
 
+impl<S: Storage2D + Clone> Clone for MortonIndex2D<S> {
+    fn clone(&self) -> Self {
+        Self { storage: self.storage.clone() }
+    }
+}
+
+impl<S: Storage2D + Copy> Copy for MortonIndex2D<S> {}
+
 impl<'a, S: Storage2D + 'a> MortonIndex2D<S> where &'a S : IntoIterator<Item = Quadrant> {
     pub fn cells(&'a self) -> <&'a S as IntoIterator>::IntoIter {
         self.storage.into_iter()
+    }
+}
+
+impl <B: FixedStorageType> MortonIndex2D<StaticStorage2D<B>> {
+    /// Returns a Morton index with the given `depth` where all cells are zeroed (i.e. representing `Quadrant::Zero`). If 
+    /// the `depth` is larger than the maximum depth of the `StaticStorage2D<B>`, `None` is returned
+    pub fn zeroed(depth: u8) -> Option<Self> {
+        if depth as usize > StaticStorage2D::<B>::MAX_LEVELS {
+            return None;
+        }
+        Some(Self {
+            storage: StaticStorage2D::<B>{
+                bits: Default::default(),
+                depth,
+            }
+        })
+    }
+
+    /// Returns a Morton index for the child `quadrant` of this Morton index. If this index is already at
+    /// the maximum depth, `None` is returned instead
+    pub fn child(&self, quadrant: Quadrant) -> Option<Self> {
+        self.storage.child(quadrant).map(|storage| Self {
+            storage,
+        })
     }
 }
 
@@ -86,12 +125,12 @@ impl <S:Storage2D> MortonIndex for MortonIndex2D<S> {
     }
 }
 
-pub trait FixedStorageType :  Bits + Copy + Clone + PartialEq + Eq + PartialOrd + Ord + Default + Debug + Hash {}
+pub trait FixedStorageType : Bits + Copy + Clone + PartialEq + Eq + PartialOrd + Ord + Default + Debug + Hash {}
 
 impl <B: Bits + Copy + Clone + PartialEq + Eq + PartialOrd + Ord + Default + Debug + Hash> FixedStorageType for B {}
 
 /// Storage for a 2D Morton index that always stores a Morton index with a fixed depth (fixed number of levels)
-#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct FixedDepthStorage2D<B: FixedStorageType> {
     bits: B,
 }
@@ -143,7 +182,7 @@ impl<B: FixedStorageType> Storage2D for FixedDepthStorage2D<B> {
 
 impl<'a, B: FixedStorageType> IntoIterator for &'a FixedDepthStorage2D<B> {
     type Item = Quadrant;
-    type IntoIter = FixedDepthIter2D<'a, B>;
+    type IntoIter = FixedDepthIter2D<'a, FixedDepthStorage2D<B>>;
 
     fn into_iter(self) -> Self::IntoIter {
         FixedDepthIter2D {
@@ -153,12 +192,12 @@ impl<'a, B: FixedStorageType> IntoIterator for &'a FixedDepthStorage2D<B> {
     }
 }
 
-pub struct FixedDepthIter2D<'a, B: FixedStorageType> {
-    storage: &'a FixedDepthStorage2D<B>,
+pub struct FixedDepthIter2D<'a, S: Storage2D> {
+    storage: &'a S,
     index: usize,
 }
 
-impl <B: FixedStorageType> Iterator for FixedDepthIter2D<'_, B>  {
+impl <S: Storage2D> Iterator for FixedDepthIter2D<'_, S>  {
     type Item = Quadrant;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -169,6 +208,121 @@ impl <B: FixedStorageType> Iterator for FixedDepthIter2D<'_, B>  {
         self.index += 1;
         unsafe {
             Some(self.storage.get_cell_at_level_unchecked(index))
+        }
+    }
+}
+
+#[derive(Default, Debug, Clone, Copy, Hash, PartialEq, Eq)]
+pub struct StaticStorage2D<B: FixedStorageType> {
+    bits: B,
+    depth: u8,
+}
+
+impl<B: FixedStorageType> StaticStorage2D<B> {
+    const MAX_LEVELS: usize = if B::BITS > 512 {
+        // Max representable value of the `u8` parameter `depth`. We could go larger by using some dependent type
+        // for storing `depth`, but it's probably not worth it at the moment
+        256
+    } else {
+        B::BITS / 2
+    };
+
+    fn child(&self, quadrant: Quadrant) -> Option<Self> {
+        if self.depth() == StaticStorage2D::<B>::MAX_LEVELS {
+            return None;
+        }
+
+        let mut ret = *self;
+        // Safe because of depth check above
+        unsafe {
+            ret.set_cell_at_level_unchecked(self.depth(), quadrant);
+        }
+        ret.depth += 1;
+        Some(ret)
+    }
+
+    fn get_cell_at_level_or_none(&self, level: usize) -> Option<Quadrant> {
+        if level >= self.depth.into() {
+            None
+        } else {
+            // Is safe because of level check above
+            unsafe {
+                Some(self.get_cell_at_level_unchecked(level))
+            }
+        }
+    }
+}
+
+impl<B: FixedStorageType> PartialOrd for StaticStorage2D<B> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<B: FixedStorageType> Ord for StaticStorage2D<B> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        let max_level = self.depth().max(other.depth());
+        for level in 0..max_level {
+            let self_cell = self.get_cell_at_level_or_none(level);
+            let other_cell = other.get_cell_at_level_or_none(level);
+            let cmp = self_cell.cmp(&other_cell);
+            if cmp == Ordering::Equal {
+                continue;
+            }
+            return cmp;
+        }
+        Ordering::Equal
+    }
+}
+
+impl<B: FixedStorageType> Storage2D for StaticStorage2D<B> {
+    fn fixed_depth() -> Option<usize> {
+        None
+    }
+
+    fn max_depth() -> Option<usize> {
+        Some(Self::MAX_LEVELS)
+    }
+
+    fn try_from_quadrants(quadrants: &[Quadrant]) -> Result<Self, crate::Error> {
+        if quadrants.len() > Self::MAX_LEVELS {
+            return Err(crate::Error::DepthLimitedExceeded {
+                max_depth: Self::MAX_LEVELS,
+            });
+        }
+        let mut ret : Self = Default::default();
+        for (level, cell) in quadrants.iter().enumerate() {
+            unsafe {
+                ret.set_cell_at_level_unchecked(level, *cell);
+            }
+        }
+        ret.depth = quadrants.len() as u8;
+        Ok(ret)
+    }
+
+    fn depth(&self) -> usize {
+        self.depth.into()
+    }
+
+    unsafe fn get_cell_at_level_unchecked(&self, level: usize) -> Quadrant {
+        let bits = self.bits.get_bits(2*level..2*(level+1)).as_u8() as usize;
+        Quadrant::try_from(bits).unwrap()
+    }
+
+    unsafe fn set_cell_at_level_unchecked(&mut self, level: usize, cell: Quadrant) {
+        let cell_index : usize = cell.into();
+        self.bits.set_bits(2*level..2*(level+1), B::from_u8(cell_index as u8));
+    }
+}
+
+impl<'a, B: FixedStorageType> IntoIterator for &'a StaticStorage2D<B> {
+    type Item = Quadrant;
+    type IntoIter = FixedDepthIter2D<'a, StaticStorage2D<B>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        FixedDepthIter2D {
+            index: 0,
+            storage: self,
         }
     }
 }
@@ -263,10 +417,104 @@ mod tests {
         }
     }
 
+    macro_rules! test_static {
+        ($typename:ident, $modname:ident, $max_levels:literal) => {
+            mod $modname {
+                use super::*;
+
+                const MAX_LEVELS: usize = $max_levels;
+
+                #[test]
+                fn default() {
+                    let idx = $typename::default();
+                    assert_eq!(0, idx.depth());
+                }
+
+                #[test]
+                fn from_quadrants() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS);
+                    let idx = $typename::try_from(quadrants.as_slice()).expect("Could not create Morton index from quadrants");
+                    assert_eq!(quadrants.len(), idx.depth());
+                    for (level, expected_quadrant) in quadrants.iter().enumerate() {
+                        assert_eq!(*expected_quadrant, idx.get_cell_at_level(level));
+                    }
+                }
+
+                #[test]
+                fn from_too_many_quadrants_fails() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS + 1);
+                    let res = $typename::try_from(quadrants.as_slice());
+                    assert!(res.is_err());
+                }
+
+                #[test]
+                fn from_fewer_quadrants_than_max_depth() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS - 2);
+                    let idx = $typename::try_from(quadrants.as_slice()).expect("Could not create Morton index from quadrants");
+                    assert_eq!(MAX_LEVELS - 2, idx.depth());
+                    for (level, expected_quadrant) in quadrants.iter().enumerate() {
+                        assert_eq!(*expected_quadrant, idx.get_cell_at_level(level));
+                    }
+                }
+
+                #[test]
+                fn set_cell() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS);
+                    let mut idx = $typename::zeroed(MAX_LEVELS as u8).unwrap();
+                    for level in 0..MAX_LEVELS {
+                        idx.set_cell_at_level(level, quadrants[level]);
+                    }
+            
+                    for level in 0..MAX_LEVELS {
+                        assert_eq!(quadrants[level], idx.get_cell_at_level(level));
+                    }
+                }
+
+                #[test]
+                #[should_panic]
+                fn set_cell_oob_panics() {
+                    let mut idx = $typename::default();
+                    // Tests both that OOB panics AND that a default-constructed Morton index with static storage has initial 
+                    // depth of zero
+                    idx.set_cell_at_level(0, Quadrant::One);
+                }
+
+                #[test]
+                fn cells_iter() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS);
+                    let idx = $typename::try_from(quadrants.as_slice()).expect("Could not create Morton index from quadrants");
+                    let collected_quadrants = idx.cells().collect::<Vec<_>>();
+                    assert_eq!(quadrants.as_slice(), collected_quadrants.as_slice());
+                }
+
+                #[test]
+                fn child() {
+                    let idx = $typename::default();
+                    let child_idx = idx.child(Quadrant::Three).expect("child() should not return None");
+                    assert_eq!(1, child_idx.depth());
+                    assert_eq!(Quadrant::Three, child_idx.get_cell_at_level(0));
+                }
+
+                #[test]
+                fn child_oob_yields_none() {
+                    let quadrants = get_test_quadrants(MAX_LEVELS);
+                    let idx = $typename::try_from(quadrants.as_slice()).expect("Could not create Morton index from quadrants");
+                    assert_eq!(None, idx.child(Quadrant::Zero));
+                }
+            }
+        }
+    }
+
     test_fixed_depth!(FixedDepthMortonIndex2D8, fixed_depth_morton_index_2d8, 4);
     test_fixed_depth!(FixedDepthMortonIndex2D16, fixed_depth_morton_index_2d16, 8);
     test_fixed_depth!(FixedDepthMortonIndex2D32, fixed_depth_morton_index_2d32, 16);
     test_fixed_depth!(FixedDepthMortonIndex2D64, fixed_depth_morton_index_2d64, 32);
     test_fixed_depth!(FixedDepthMortonIndex2D128, fixed_depth_morton_index_2d128, 64);
+
+    test_static!(StaticMortonIndex2D8, static_morton_index_2d8, 4);
+    test_static!(StaticMortonIndex2D16, static_morton_index_2d16, 8);
+    test_static!(StaticMortonIndex2D32, static_morton_index_2d32, 16);
+    test_static!(StaticMortonIndex2D64, static_morton_index_2d64, 32);
+    test_static!(StaticMortonIndex2D128, static_morton_index_2d128, 64);
 
 }
